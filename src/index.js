a25bba8d287ecc77f4845ce9db6ef569af94ec61
Kelsey Jackson <kelseyjackson@kelseys-mbp.attlocal.net>
2021-03-20 15:41:45 -0500

    add redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import { searchRobots } from './reducers';
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
 import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const logger = createLogger();
+const rootReducer = combineReducers({ searchRobots, requestRobots });
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-  searchRobots,
-  composeEnhancers(applyMiddleware(logger))
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk, logger))
 );
 ReactDOM.render(
   <React.StrictMode>
